c254b1affceb9af3e555f065af88190c765b1dcb
eric11598 <eric.arellano.0803@gmail.com>
2018-09-18 01:01:29 -0400

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,22 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendsData = require("./../data/friends.js");
+var friends = require("./../data/friends.js");
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+  app.get("/api/friends", function (req, res) {
+    res.json(friends);
   });
 
 
@@ -31,9 +31,43 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-    res.json(true);
+  app.post("/api/friends", function (req, res) {
+    
+    
 
-})
+    var bestFriend = {
+      name: "",
+      photo: "",
+      friendDifference: 500
+    };
+    
+    var userData = req.body;
+    var userScores = userData.scores;
+
+
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      var total = 0;
+
+      for (var x = 0; x < currentFriend.scores.length; x++) {
+
+        total += Math.abs(parseInt(userData.scores[x]) - parseInt(currentFriend.scores[x]));
+      }
+      
+      if (total <= bestFriend.friendDifference) {
+
+
+        bestFriend = {
+          name: currentFriend.name,
+          photo: currentFriend.photo,
+          friendDifference: total
+        };
+
+
+      }
+    }
+
+    friends.push(req.body);
+    res.json(bestFriend);
+  })
 }
